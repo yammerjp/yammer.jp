508c91faa776a870a6a4f49ddb2bd1d310ccf284
Keisuke Nakayama <me@yammer.jp>
2024-08-20 01:24:35 +0900

    fix hydration mismatch

--- app/components/FeedItemCard.tsx
+++ app/components/FeedItemCard.tsx
@@ -29,10 +29,16 @@ function description(str: string) {
   return `${str.slice(0, maxLength - 1)}...`;
 }
 
-function iso8601toStr(isoDate: string): string {
-  const d = new Date(isoDate);
-  const yyyy = `${d.getFullYear()}`;
-  const mm = (`00${d.getMonth() + 1}`).slice(-2);
-  const dd = (`00${d.getDate()}`).slice(-2);
-  return `${yyyy}/${mm}/${dd}`;
+const iso8601toStr = (iso8601: string | undefined) => {
+  if (iso8601 === undefined) {
+    return ''
+  }
+  // 実行環境のタイムゾーンに関わらず、日本時間の値を取り出す
+  const date = new Date(iso8601)
+  const timezoneoffset = -9 // UTC-表示したいタイムゾーン(単位:hour)。JSTなら-9
+  const fakeUTC = new Date(date.getTime() - (timezoneoffset * 60 - new Date().getTimezoneOffset()) * 60000)
+  const yyyy = '' + fakeUTC.getFullYear()
+  const MM = ('00' + (fakeUTC.getMonth() + 1)).slice(-2)
+  const dd = ('00' + fakeUTC.getDate()).slice(-2)
+  return `${yyyy}/${MM}/${dd}`
 }
