a9b45d29d891cfb3856e2780e1f32b900109fb1c
Keisuke Nakayama <me@yammer.jp>
2024-08-19 20:52:25 +0900

    add /feeds/slides

---
+++ app/routes/feeds.slides.tsx
@@ -0,0 +1,41 @@
+import type { MetaFunction } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+
+import { FeedItemCards } from "../components/FeedItemCards";
+import { TabSelector } from "../components/TabSelector";
+import { transformFeeds } from "../utils/FeedTransformer";
+import type { JsonFeedItem } from "../types/JsonFeedItem";
+import slides from "../data/slides.json";
+
+export async function loader() {
+  return json(
+    await loadArticles()
+  );
+}
+
+async function loadArticles(): Promise<{message: string, items: JsonFeedItem[]}> {
+  const feeds = await transformFeeds(slides);
+  if (feeds.length > 0) {
+    return {message: "", items: feeds};
+  } else {
+    return {message: "No posts found", items: []};
+  }
+
+}
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "発表 - yammer.jp" },
+  ];
+};
+
+export default function Index() {
+  const {message, items} = useLoaderData<typeof loader>();
+  return (
+    <div>
+      <TabSelector selected="発表" />
+      <FeedItemCards items={items} message={message} />
+    </div>
+  );
+}
